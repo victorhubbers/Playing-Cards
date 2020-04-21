0e0db2f1f37311ec6e05fbb467f832f9112bfff3
victorhubbers <469487@student.saxion.nl>
2020-04-21 15:47:59 +0200

    included <vue-playing-card /> component

--- src/main.js
+++ src/main.js
@@ -1,9 +1,10 @@
 import Vue from "vue";
 import App from "./App.vue";
 import store from "./store";
+import VuePlayingCard from "vue-playing-card";
 
 Vue.config.productionTip = false;
-
+Vue.use(VuePlayingCard);
 new Vue({
   store,
   render: h => h(App)
