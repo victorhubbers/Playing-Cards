3b67f323b6e4728cf111f7538b7f741bb6f4bf90
victorhubbers <469487@student.saxion.nl>
2020-04-22 13:43:10 +0200

    added shuffle algorithm (fisherYates) and stripped useless stuff

--- cards-api/server/server.js
+++ cards-api/server/server.js
@@ -2,41 +2,34 @@ import bodyParser from "body-parser";
 import express from "express";
 import cors from "cors";
 
-import cards from "./cards-init.js";
-
 const app = express();
-
-// Setup the body parser
 app.use(bodyParser.json());
 app.use(cors());
-// Data store (in memory)
-const todoList = [];
 
-app.get("/api/todo", function(req, res) {
+//import the deck, which is not shuffled yet.
+import cards from "./cards-init.js";
+let deck = shuffleFisherYates(cards);
+
+//(re)shuffle the deck
+
+//draw card(s) from the deck
+app.get("deck/card", function(req, res) {
   res.status(200);
-  res.json(todoList.getList());
+  res.json();
 });
 
-app.post("/api/todo/:todoItem", function(req, res) {
-  try {
-    todoList.add(req.params.todoItem);
-    res.status(201);
-    res.end();
-  } catch (err) {
-    res.status(400);
-    res.end();
-  }
+//return cards to the deck
+app.post("deck/cards", function(req, res) {
+  deck = shuffleFisherYates(deck);
 });
 
-app.delete("/api/todo/:todoItem", function(req, res) {
-  try {
-    todoList.delete(req.params.todoItem);
-    res.status(204);
-    res.end();
-  } catch (err) {
-    res.status(404);
-    res.end();
+function shuffleFisherYates(deck) {
+  let i = deck.length;
+  while (i--) {
+    const ri = Math.floor(Math.random() * (i + 1));
+    [deck[i], deck[ri]] = [deck[ri], deck[i]];
   }
-});
+  return deck;
+}
 
 export default app;
