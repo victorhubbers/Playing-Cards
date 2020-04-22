b0cc8fe643c0961e4e977b2467945d73e5fcb61d
victorhubbers <469487@student.saxion.nl>
2020-04-22 13:26:16 +0200

    added signatures to cards-init.js

--- cards-api/server/cards-init.js
+++ cards-api/server/cards-init.js
@@ -1,211 +1,263 @@
 const cards = [
   {
     suit: "hearts",
-    value: 2
+    value: 2,
+    signature: "2h"
   },
   {
     suit: "hearts",
-    value: 3
+    value: 3,
+    signature: "3h"
   },
   {
     suit: "hearts",
-    value: 4
+    value: 4,
+    signature: "4h"
   },
   {
     suit: "hearts",
-    value: 5
+    value: 5,
+    signature: "5h"
   },
   {
     suit: "hearts",
-    value: 6
+    value: 6,
+    signature: "6h"
   },
   {
     suit: "hearts",
-    value: 7
+    value: 7,
+    signature: "7h"
   },
   {
     suit: "hearts",
-    value: 8
+    value: 8,
+    signature: "8h"
   },
   {
     suit: "hearts",
-    value: 9
+    value: 9,
+    signature: "9h"
   },
   {
     suit: "hearts",
-    value: 10
+    value: 10,
+    signature: "10h"
   },
   {
     suit: "hearts",
-    value: "J"
+    value: "Jack",
+    signature: "jh"
   },
   {
     suit: "hearts",
-    value: "Q"
+    value: "Queen",
+    signature: "qh"
   },
   {
     suit: "hearts",
-    value: "K"
+    value: "King",
+    signature: "kh"
   },
   {
     suit: "hearts",
-    value: "A"
+    value: "Ace",
+    signature: "ah"
   },
   {
     suit: "diamonds",
-    value: 2
+    value: 2,
+    signature: "2d"
   },
   {
     suit: "diamonds",
-    value: 3
+    value: 3,
+    signature: "3d"
   },
   {
     suit: "diamonds",
-    value: 4
+    value: 4,
+    signature: "4d"
   },
   {
     suit: "diamonds",
-    value: 5
+    value: 5,
+    signature: "5d"
   },
   {
     suit: "diamonds",
-    value: 6
+    value: 6,
+    signature: "6d"
   },
   {
     suit: "diamonds",
-    value: 7
+    value: 7,
+    signature: "7d"
   },
   {
     suit: "diamonds",
-    value: 8
+    value: 8,
+    signature: "8d"
   },
   {
     suit: "diamonds",
-    value: 9
+    value: 9,
+    signature: "9d"
   },
   {
     suit: "diamonds",
-    value: 10
+    value: 10,
+    signature: "10d"
   },
   {
     suit: "diamonds",
-    value: "J"
+    value: "Jack",
+    signature: "jd"
   },
   {
     suit: "diamonds",
-    value: "Q"
+    value: "Queen",
+    signature: "qd"
   },
   {
     suit: "diamonds",
-    value: "K"
+    value: "King",
+    signature: "kd"
   },
   {
     suit: "diamonds",
-    value: "A"
+    value: "Ace",
+    signature: "ad"
   },
   {
     suit: "clubs",
-    value: 2
+    value: 2,
+    signature: "2c"
   },
   {
     suit: "clubs",
-    value: 3
+    value: 3,
+    signature: "3c"
   },
   {
     suit: "clubs",
-    value: 4
+    value: 4,
+    signature: "4c"
   },
   {
     suit: "clubs",
-    value: 5
+    value: 5,
+    signature: "5c"
   },
   {
     suit: "clubs",
-    value: 6
+    value: 6,
+    signature: "6c"
   },
   {
     suit: "clubs",
-    value: 7
+    value: 7,
+    signature: "7c"
   },
   {
     suit: "clubs",
-    value: 8
+    value: 8,
+    signature: "8c"
   },
   {
     suit: "clubs",
-    value: 9
+    value: 9,
+    signature: "9c"
   },
   {
     suit: "clubs",
-    value: 10
+    value: 10,
+    signature: "10c"
   },
   {
     suit: "clubs",
-    value: "J"
+    value: "Jack",
+    signature: "jc"
   },
   {
     suit: "clubs",
-    value: "Q"
+    value: "Queen",
+    signature: "qc"
   },
   {
     suit: "clubs",
-    value: "K"
+    value: "King",
+    signature: "kc"
   },
   {
     suit: "clubs",
-    value: "A"
+    value: "Ace",
+    signature: "ac"
   },
   {
     suit: "spades",
-    value: 2
+    value: 2,
+    signature: "2s"
   },
   {
     suit: "spades",
-    value: 3
+    value: 3,
+    signature: "3s"
   },
   {
     suit: "spades",
-    value: 4
+    value: 4,
+    signature: "4s"
   },
   {
     suit: "spades",
-    value: 5
+    value: 5,
+    signature: "5s"
   },
   {
     suit: "spades",
-    value: 6
+    value: 6,
+    signature: "6s"
   },
   {
     suit: "spades",
-    value: 7
+    value: 7,
+    signature: "7s"
   },
   {
     suit: "spades",
-    value: 8
+    value: 8,
+    signature: "8s"
   },
   {
     suit: "spades",
-    value: 9
+    value: 9,
+    signature: "9s"
   },
   {
     suit: "spades",
-    value: 10
+    value: 10,
+    signature: "10s"
   },
   {
     suit: "spades",
-    value: "J"
+    value: "Jack",
+    signature: "js"
   },
   {
     suit: "spades",
-    value: "Q"
+    value: "Queen",
+    signature: "qs"
   },
   {
     suit: "spades",
-    value: "K"
+    value: "King",
+    signature: "ks"
   },
   {
     suit: "spades",
-    value: "A"
+    value: "Ace",
+    signature: "as"
   }
 ];
 
