8a2ff418434ec9e51084fe0c5f7e3420daec8e34
victorhubbers <469487@student.saxion.nl>
2020-04-24 23:28:35 +0200

    added nice divider

--- src/components/HigherLower.vue
+++ src/components/HigherLower.vue
@@ -7,6 +7,9 @@
       size="50"
       >mdi-arrow-up-bold</v-icon
     >
+    <v-layout style="padding-top: 10px">
+      <v-divider class="child"></v-divider>
+    </v-layout>
     <v-icon @click="guessLower(payload)" class="child" color="#F44336" size="50"
       >mdi-arrow-down-bold</v-icon
     >
@@ -38,7 +41,7 @@ export default {
   display: inline-grid;
   width: 94px;
   border-radius: 5px;
-  border: 3px dashed rgba(100, 100, 100, 1);
+  border: 2px dashed rgba(100, 100, 100, 1);
   background: rgba(211, 211, 211, 0.6);
 }
 #hoverWrapper {
